e91a45774dd53f76fa433abe8cc7b1e8a6e4dc1a
CSITDETO <Daniele.Tagliaferro@creditsafe.it>
2020-05-27 18:15:09 +0200

    Fixed some references

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -14,13 +14,14 @@ class Calculator():
     STATUS_INVALID_EXPR = 'INVALID_EXPR'
     STATUS_INVALID_IDENT = 'INVALID_IDENT'
     STATUS_INVALID_ASSIGN = 'INVALID_ASSIGN'
+    STATUS_UNKNOWN_CMD = 'UNKNOWN_CMD'
     TYPE_NUMBER = 'NUMBER'
     TYPE_TOKEN = 'TOKEN'
     TYPE_OPERATOR = 'OPERATOR'
 
-    def show_help(cls):
-        print("""The program is able to perform some calculations
-It can calculates sums and differences of numbers""")
+    def show_help(self):
+        print(f"The {self.__class__.__name___} is able to perform some calculations\n" +
+              "It can calculates sums and differences of numbers")
 
     def __init__(self):
         self.result = None
@@ -39,47 +40,54 @@ It can calculates sums and differences of numbers""")
             input_line = self.get_inputs()
             if input_line[0] == '/':
                 command = input_line[1:]
-                self.exec_command(command)
+                self.result = self.exec_command(command)
             elif '=' in input_line:
                 self.result = self.parse_assignment(input_line)
             elif input_line:
                 self.result = self.eval_expression(input_line)
 
-            if self.result == Calculator.STATUS_INVALID_ASSIGN:
-                print('Invalid assignment')
-            elif self.result == Calculator.STATUS_UNKNOWN_TOKEN:
-                print('Unknown variable')
-            elif self.result == Calculator.STATUS_INVALID_TOKEN:
-                print('Invalid identifier')
-            elif self.result == Calculator.STATUS_INVALID_IDENT:
-                print('Invalid identifier')
-            elif self.result is None:
-                pass
-            else:
-                print(self.result)
+            self.show_output()
         print('Bye!')
 
-    def exec_command(self, command):
-        if command not in Calculator.VALID_COMMANDS:
+    def show_output(self):
+        if self.result == self.STATUS_INVALID_ASSIGN:
+            print('Invalid assignment')
+        elif self.result == self.STATUS_UNKNOWN_TOKEN:
+            print('Unknown variable')
+        elif self.result == self.STATUS_INVALID_TOKEN:
+            print('Invalid identifier')
+        elif self.result == self.STATUS_INVALID_IDENT:
+            print('Invalid identifier')
+        elif self.result == self.STATUS_UNKNOWN_CMD:
             print("Unknown command")
+        elif self.result is None:
+            pass
+        else:
+            print(self.result)
+
+    def exec_command(self, command):
+        result = None
+        if command not in self.VALID_COMMANDS:
+            result = self.STATUS_UNKNOWN_CMD
         elif command == 'exit':
-            self.result = None
+            pass
         elif command == 'help':
-            Calculator.show_help()
+            self.show_help()
+        return result
 
     def parse_assignment(self, input):
         sides = input.split('=')
         token_id = sides[0].strip()
         if len(sides) != 2:
-            return Calculator.STATUS_INVALID_ASSIGN
+            return self.STATUS_INVALID_ASSIGN
         if not self.valid_token(token_id):
-            return Calculator.STATUS_INVALID_IDENT
+            return self.STATUS_INVALID_IDENT
         key = token_id
         value = self.eval_expression(sides[1])
-        if value == Calculator.STATUS_UNKNOWN_TOKEN:
+        if value == self.STATUS_UNKNOWN_TOKEN:
             print('Unknown variable')
-        elif value == Calculator.STATUS_INVALID_TOKEN:
-            return Calculator.STATUS_INVALID_ASSIGN
+        elif value == self.STATUS_INVALID_TOKEN:
+            return self.STATUS_INVALID_ASSIGN
         else:
             self.local_vars[key] = value
 
@@ -87,30 +95,30 @@ It can calculates sums and differences of numbers""")
     def parse_expression(self, input):
         i = 0
         items = []
-        status = Calculator.STATUS_VALID
+        status = self.STATUS_VALID
         while i < len(input):
             char = input[i]
             if char == ' ':
                 i += 1
-            elif char in Calculator.CHAR_OPERATORS:
-                items.append((Calculator.TYPE_OPERATOR, char))
+            elif char in self.CHAR_OPERATORS:
+                items.append((self.TYPE_OPERATOR, char))
                 i += 1
             else:
                 str_item = ''
-                while i < len(input) and input[i] not in Calculator.CHAR_SEPARATORS:
+                while i < len(input) and input[i] not in self.CHAR_SEPARATORS:
                     str_item += input[i]
                     i += 1
                 if str_item.isnumeric():
-                    items.append((Calculator.TYPE_NUMBER, int(str_item)))
+                    items.append((self.TYPE_NUMBER, int(str_item)))
                 elif self.valid_token(str_item):
                     if str_item not in self.local_vars:
-                        items.append((Calculator.TYPE_TOKEN, str_item))
-                        status = Calculator.STATUS_UNKNOWN_TOKEN
+                        items.append((self.TYPE_TOKEN, str_item))
+                        status = self.STATUS_UNKNOWN_TOKEN
                     else:
-                        items.append((Calculator.TYPE_TOKEN, str_item))
+                        items.append((self.TYPE_TOKEN, str_item))
                 else:
-                    items.append((Calculator.TYPE_TOKEN, str_item))
-                    status = Calculator.STATUS_INVALID_TOKEN
+                    items.append((self.TYPE_TOKEN, str_item))
+                    status = self.STATUS_INVALID_TOKEN
 
         # print({'status': status, 'items': items})
         return {'status': status, 'items': items}
@@ -135,7 +143,7 @@ It can calculates sums and differences of numbers""")
 
 
     def process_item(self, item, itemtype, state, result):
-        if itemtype == Calculator.TYPE_NUMBER:
+        if itemtype == self.TYPE_NUMBER:
             if state == 'START':
                 result = item
                 state = 'RESULT'
@@ -147,7 +155,7 @@ It can calculates sums and differences of numbers""")
             elif state == 'SUB':
                 result -= item
                 state = 'RESULT'
-        elif itemtype == Calculator.TYPE_OPERATOR:
+        elif itemtype == self.TYPE_OPERATOR:
             if item == '+':
                 if state in ['START', 'RESULT', 'ADD']:
                     state = 'ADD'
@@ -160,10 +168,10 @@ It can calculates sums and differences of numbers""")
                     state = 'ADD'
             else:
                 state = 'ERROR'
-        elif itemtype == Calculator.TYPE_TOKEN:
+        elif itemtype == self.TYPE_TOKEN:
             if item in self.local_vars:
                 item = self.local_vars[item]
-                state, result = self.process_item(item, Calculator.TYPE_NUMBER, state, result)
+                state, result = self.process_item(item, self.TYPE_NUMBER, state, result)
 
 
 
@@ -171,7 +179,7 @@ It can calculates sums and differences of numbers""")
 
     def valid_token(self, token):
         for char in token.lstrip().lower():
-            if char not in Calculator.CHAR_LETTERS:
+            if char not in self.CHAR_LETTERS:
                 return False
         return True
 
